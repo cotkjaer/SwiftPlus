7f570fa07de8476b9c273c76c3b492a8b7fa960f
christian otkjær <christian.otkjaer@gmail.com>
2016-10-24 10:30:11 +0200

    Initial Commit

---
+++ SwiftPlus/SwiftPlus.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPlus.h
+//  SwiftPlus
+//
+//  Created by Christian Otkjær on 24/10/16.
+//  Copyright © 2016 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftPlus.
+FOUNDATION_EXPORT double SwiftPlusVersionNumber;
+
+//! Project version string for SwiftPlus.
+FOUNDATION_EXPORT const unsigned char SwiftPlusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPlus/PublicHeader.h>
+
+
